3b49a342bb8b1098d2132a43bb7acb978f353142
jamshidnoor <44399792+jamshidnoor@users.noreply.github.com>
2023-01-05 13:09:56 -0500

    Update ReviewClass.java
    Main method and Review method removed

--- src/review/ReviewClass.java
+++ src/review/ReviewClass.java
@@ -2,11 +2,4 @@ package review;
 
 public class ReviewClass {
 
-	public static void main(String[] args) {
 	}
-	
-	public void test() {
-		System.out.println("this is method is on review session branch");
-	}
-
-}
